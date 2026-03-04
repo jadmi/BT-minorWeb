ebe755849b628204b0e2879022e270c57bc7fcfe
Justin <justin.admiraal@hva.nl>
2026-03-04 09:33:28 +0100

    checkout week2 dag2
    - structuur aangepast, verder met styling, begin validation

--- js/formvalidity.js
+++ js/formvalidity.js
@@ -1,15 +1,53 @@
-const bsn = document.getElementById("bsnNumber");
-const form = document.querySelector("form");
+// validation:
+// Victor
+// https://www.youtube.com/watch?v=h5qqmE83Tes
 
-bsn.addEventListener("input", (event) => {
-  if (bsn.validity.valueMissing) {
-    bsn.setCustomValidity("Dit veld is verplicht!");
-  } else if (bsn.validity.patternMismatch) {
-    bsn.setCustomValidity("Dit veld moet 8 of 9 cijfers bevatten.");
+const form = document.querySelector("form");
+const formGroup1 = document.querySelectorAll(".personInfo input");
+
+form.setAttribute("novalidate", true);
+
+function validateField(field) {
+  if (!field.validity.valid) {
+    console.log("field is invalid");
+    return false;
   } else {
-    bsn.setCustomValidity("");
+    return true;
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let isValid = true;
+
+  const fields = formGroup1;
+  fields.forEach((field) => {
+    console.log(`Checking ${field.name}`);
+    const fieldValid = validateField(field);
+    if (!fieldValid) {
+      isValid = false;
+    } else {
+      console.log("valide veld");
+    }
+  });
+
+  if (isValid) {
+    console.log("submitting");
+  } else {
+    console.log("error");
   }
 });
+
+// bsn.addEventListener("input", (event) => {
+//   if (bsn.validity.valueMissing) {
+//     bsn.setCustomValidity("Dit veld is verplicht!");
+//   } else if (bsn.validity.patternMismatch) {
+//     bsn.setCustomValidity("Dit veld moet 8 of 9 cijfers bevatten.");
+//   } else {
+//     bsn.setCustomValidity("");
+//   }
+// });
 
 // form.addEventListener("submit", function (event) {
 //   event.preventDefault();
