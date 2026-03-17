1f02789200b43556a7760fac84f087147b7e10e3
Justin <justin.admiraal@hva.nl>
2026-03-17 21:09:41 +0100

    Pattern verkrijgers added, @starting style transitions

--- js/formvalidity.js
+++ js/formvalidity.js
@@ -7,6 +7,7 @@ const input = document.querySelectorAll("input");
 const fileInput = document.querySelector("#deed");
 const fileName = document.querySelector("#fileName");
 const today = new Date().toISOString().split("T")[0];
+const beneficiary = document.querySelector(".beneficiary");
 
 form.setAttribute("novalidate", true);
 
@@ -144,23 +145,134 @@ function hideErrorSummary() {
   errorSummaryList.innerHTML = "";
 }
 
-const groupInputs = document.querySelectorAll("#representativeGroup input");
-groupInputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    if (input.value.length > 0) {
-      groupInputs.forEach((other) => {
-        if (other !== input) {
-          other.disabled = true;
-          other.required = false;
-        } else {
-          other.required = true;
-        }
-      });
-    } else {
-      groupInputs.forEach((other) => {
-        other.disabled = false;
-        other.required = false;
-      });
+const container = document.querySelector("#beneficiaryContainer");
+
+container.addEventListener("click", function (event) {
+  if (event.target.classList.contains("beneficiary")) {
+    addBeneficiary();
+  }
+});
+
+let beneficiaryCount = 1;
+function addBeneficiary() {
+  beneficiaryCount++;
+  const html = `
+    <div class="optionalQuestion">
+      <fieldset class="nestedFieldset">
+        <legend class="followUpQuestion">Voer de gegevens in van verkrijger ${beneficiaryCount}</legend>
+
+        <p class="errorMessage" aria-live="polite" id="lastNameError${beneficiaryCount}"></p>
+
+        <label for="bsnNumber${beneficiaryCount}">BSN of RSIN verkrijger </label>
+        <input
+          type="text"
+          id="bsnNumber${beneficiaryCount}"
+          name="bsnNummer${beneficiaryCount}"
+          inputmode="numeric"
+          minlength="8"
+          maxlength="9"
+          pattern="\\d{8,9}"
+          required
+          data-error="Vul een geldig BSN of RSIN in van 8 of 9 cijfers!"
+        />
+        <p class="errorMessage" aria-live="polite" id="bsnNumberError${beneficiaryCount}"></p>
+
+        <label for="firstNameBeneficiary${beneficiaryCount}">Voorletter(s) </label>
+        <input
+          type="text"
+          id="firstNameBeneficiary${beneficiaryCount}"
+          name="voornaamNotaris${beneficiaryCount}"
+          pattern="^[A-Za-z.]+$"
+          data-error="Vul alleen letters in bij voorletters!"
+        />
+
+        <label for="prefixBeneficiary${beneficiaryCount}">Tussenvoegsel(s) </label>
+        <input
+          type="text"
+          id="prefixBeneficiary${beneficiaryCount}"
+          name="tussenvoegselVerkrijger${beneficiaryCount}"
+        />
+
+        <label for="lastNameBeneficiary${beneficiaryCount}">Achternaam</label>
+        <input
+          type="text"
+          id="lastNameBeneficiary${beneficiaryCount}"
+          name="achternaamVerkrijger${beneficiaryCount}"
+          pattern="^[A-Za-z.]+$"
+          data-error="Vul alleen letters in bij de achternaam van de verkrijger!"
+        />
+
+        <fieldset>
+          <legend class="followUpQuestion">
+            Krijgt deze verkrijger waarvoor u geen aangifte doet het hele vermogen?
+          </legend>
+          <label class="radioWrapper">
+            <input type="radio" name="verkrijgerVraag1-${beneficiaryCount}" value="ja" />
+            Ja
+          </label>
+          <label class="radioWrapper">
+            <input type="radio" name="verkrijgerVraag1-${beneficiaryCount}" value="nee" />
+            Nee
+          </label>
+        </fieldset>
+
+        <fieldset class="lastFieldset">
+          <legend class="followUpQuestion" >
+            Doet deze verkrijger een beroep op diens wettelijke erfdeel?
+          </legend>
+          <label class="radioWrapper">
+            <input type="radio" name="verkrijgerVraag2-${beneficiaryCount}" value="ja" />
+            Ja
+          </label>
+          <label class="radioWrapper">
+            <input type="radio" name="verkrijgerVraag2-${beneficiaryCount}" value="nee" />
+            Nee
+          </label>
+
+        </fieldset>
+      </fieldset>
+    </div>
+  `;
+
+  container.insertAdjacentHTML("beforeend", html);
+
+  const button = container.querySelector(".beneficiary");
+
+  // Einde van formulier verkrijger pakken en daaronder de knop plaatsen
+  const allQuestions = container.querySelectorAll(".optionalQuestion");
+  const newestQuestion = allQuestions[allQuestions.length - 1];
+  const newestLastFieldset = newestQuestion.querySelector(".lastFieldset");
+  newestLastFieldset.appendChild(button);
+}
+
+document.querySelectorAll("input[type='radio']").forEach((radio) => {
+  radio.addEventListener("change", (e) => {
+    if (e.target.name === "verkrijger") {
+      e.target
+        .closest(".radioWrapper")
+        .scrollIntoView({ behavior: "smooth", block: "nearest" });
+      return;
+    }
+
+    const optionalQuestion =
+      e.target.closest(".optionalChoice")?.nextElementSibling;
+
+    const radioWrapper = e.target
+      .closest(".radioWrapper")
+      ?.parentElement?.querySelector(".fileLabel, .marriageDiv");
+
+    const target = optionalQuestion || radioWrapper;
+
+    if (target && target.style.display !== "none") {
+      target.scrollIntoView({ behavior: "smooth", block: "nearest" });
     }
   });
+});
+
+document.querySelector(".beneficiary").addEventListener("click", () => {
+  setTimeout(() => {
+    const allBeneficiaries = document.querySelectorAll(".nestedFieldset");
+    const last = allBeneficiaries[allBeneficiaries.length - 1];
+    last.scrollIntoView({ behavior: "smooth", block: "start" });
+  }, 50);
 });
