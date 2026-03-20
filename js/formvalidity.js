d023e4fefd8d81953c6030e989691a040bdce21a
jadmi <152192404+jadmi@users.noreply.github.com>
2026-03-20 09:14:05 +0100

    Fixes, cleanup, comments

--- js/formvalidity.js
+++ js/formvalidity.js
@@ -1,20 +1,21 @@
 // validation:
 // Victor
-// https://www.youtube.com/watch?v=h5qqmE83Tes
+// https://www.youtube.com/watch?v=h5qqmE83Tes !!
 
 const form = document.querySelector("form");
 const input = document.querySelectorAll("input");
 const fileInput = document.querySelector("#deed");
 const fileName = document.querySelector("#fileName");
+// https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
 const today = new Date().toISOString().split("T")[0];
 const beneficiary = document.querySelector(".beneficiary");
 
 form.setAttribute("novalidate", true);
 
 // https://stackoverflow.com/questions/574904/get-next-previous-element-using-javascript
+// basis form validatie
 function validateField(field) {
   const label = document.querySelector(`label[for="${field.id}"]`);
-  if (field.closest(".nestedFieldset")) return true;
 
   const errorMessage =
     field.type === "radio"
@@ -43,6 +44,8 @@ function validateField(field) {
 
     field.removeAttribute("aria-describedby");
 
+    // geneste velden negeren bij validatie
+    // developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent
     if (field.type === "radio") {
       if (field.closest(".optionalQuestion")) return true;
       if (field.closest(".nestedFieldset")) return true;
@@ -85,7 +88,7 @@ form.addEventListener("submit", function (e) {
       ) {
         return;
       }
-      // Hulp van claude met array method
+      //developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
       invalidFields.push(field);
     }
   });
@@ -106,6 +109,7 @@ document.querySelectorAll('input[type="date"]').forEach((input) => {
   input.max = today;
 });
 
+// Error samenvatting bovenaan pagina
 function showErrorSummary(invalidFields) {
   const errorSummary = document.querySelector("#errorSummary");
   const errorSummaryList = document.querySelector("#errorSummaryList");
@@ -132,7 +136,7 @@ function showErrorSummary(invalidFields) {
     li.appendChild(link);
     errorSummaryList.appendChild(li);
   });
-
+  // developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden
   errorSummary.hidden = false;
   errorSummary.focus();
 }
@@ -153,6 +157,8 @@ container.addEventListener("click", function (event) {
   }
 });
 
+// Verkrijgers toevoegen, html toevoegen
+
 let beneficiaryCount = 1;
 function addBeneficiary() {
   beneficiaryCount++;
@@ -161,74 +167,76 @@ function addBeneficiary() {
       <fieldset class="nestedFieldset">
         <legend class="followUpQuestion">Voer de gegevens in van verkrijger ${beneficiaryCount}</legend>
 
-        <p class="errorMessage" aria-live="polite" id="lastNameError${beneficiaryCount}"></p>
-
-        <label for="bsnNumber${beneficiaryCount}">BSN of RSIN verkrijger </label>
+        <label for="bsnNumberBeneficiary">BSN of RSIN verkrijger</label>
         <input
           type="text"
-          id="bsnNumber${beneficiaryCount}"
-          name="bsnNummer${beneficiaryCount}"
+          id="bsnNumberBeneficiary"
+          name="bsnNummerVerkrijger"
           inputmode="numeric"
           minlength="8"
           maxlength="9"
           pattern="\\d{8,9}"
-          required
           data-error="Vul een geldig BSN of RSIN in van 8 of 9 cijfers!"
+          title="Vul een geldig BSN of RSIN in van 8 of 9 cijfers!"
         />
-        <p class="errorMessage" aria-live="polite" id="bsnNumberError${beneficiaryCount}"></p>
+        <p class="errorMessage" aria-live="polite"></p>
 
-        <label for="firstNameBeneficiary${beneficiaryCount}">Voorletter(s) </label>
+        <label for="firstNameBeneficiary">Voorletter(s)</label>
         <input
           type="text"
-          id="firstNameBeneficiary${beneficiaryCount}"
-          name="voornaamNotaris${beneficiaryCount}"
+          id="firstNameBeneficiary"
+          name="voornaamVerkrijger"
           pattern="^[A-Za-z.]+$"
-          data-error="Vul alleen letters in bij voorletters!"
+          data-error="Vul alleen letters in bij de voorletters!"
+          title="Vul alleen letters in bij de voorletters!"
         />
+        <p class="errorMessage" aria-live="polite"></p>
 
-        <label for="prefixBeneficiary${beneficiaryCount}">Tussenvoegsel(s) </label>
+        <label for="prefixBeneficiary">Tussenvoegsel(s) - niet verplicht</label>
         <input
           type="text"
-          id="prefixBeneficiary${beneficiaryCount}"
-          name="tussenvoegselVerkrijger${beneficiaryCount}"
+          id="prefixBeneficiary"
+          name="tussenvoegselVerkrijger"
         />
+        <p class="errorMessage" aria-live="polite"></p>
 
-        <label for="lastNameBeneficiary${beneficiaryCount}">Achternaam</label>
+        <label for="lastNameBeneficiary">Achternaam</label>
         <input
           type="text"
-          id="lastNameBeneficiary${beneficiaryCount}"
-          name="achternaamVerkrijger${beneficiaryCount}"
+          id="lastNameBeneficiary"
+          name="achternaamVerkrijger"
           pattern="^[A-Za-z.]+$"
           data-error="Vul alleen letters in bij de achternaam van de verkrijger!"
+          title="Vul alleen letters in bij de achternaam van de verkrijger!"
         />
+        <p class="errorMessage" aria-live="polite"></p>
 
-        <fieldset>
+        <fieldset class="nestedFieldset">
           <legend class="followUpQuestion">
             Krijgt deze verkrijger waarvoor u geen aangifte doet het hele vermogen?
           </legend>
           <label class="radioWrapper">
-            <input type="radio" name="verkrijgerVraag1-${beneficiaryCount}" value="ja" />
+            <input type="radio" name="verkrijgerVraag1" value="ja" />
             Ja
           </label>
           <label class="radioWrapper">
-            <input type="radio" name="verkrijgerVraag1-${beneficiaryCount}" value="nee" />
+            <input type="radio" name="verkrijgerVraag1" value="nee" />
             Nee
           </label>
         </fieldset>
 
-        <fieldset class="lastFieldset">
-          <legend class="followUpQuestion" >
+        <fieldset class="lastFieldset nestedFieldset">
+          <legend class="followUpQuestion">
             Doet deze verkrijger een beroep op diens wettelijke erfdeel?
           </legend>
           <label class="radioWrapper">
-            <input type="radio" name="verkrijgerVraag2-${beneficiaryCount}" value="ja" />
+            <input type="radio" name="verkrijgerVraag2" value="ja" />
             Ja
           </label>
           <label class="radioWrapper">
-            <input type="radio" name="verkrijgerVraag2-${beneficiaryCount}" value="nee" />
+            <input type="radio" name="verkrijgerVraag2" value="nee" />
             Nee
           </label>
-
         </fieldset>
       </fieldset>
     </div>
@@ -245,6 +253,8 @@ function addBeneficiary() {
   newestLastFieldset.appendChild(button);
 }
 
+// Hulp van victor, scroll into view per vraag bij progressive disclosure
+//developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
 document.querySelectorAll("input[type='radio']").forEach((radio) => {
   radio.addEventListener("change", (e) => {
     if (e.target.name === "verkrijger") {
@@ -255,11 +265,11 @@ document.querySelectorAll("input[type='radio']").forEach((radio) => {
     }
 
     const optionalQuestion =
-      e.target.closest(".optionalChoice")?.nextElementSibling;
+      e.target.closest(".optionalChoice").nextElementSibling;
 
     const radioWrapper = e.target
       .closest(".radioWrapper")
-      ?.parentElement?.querySelector(".fileLabel, .marriageDiv");
+      .parentElement.querySelector(".fileLabel, .marriageDiv");
 
     const target = optionalQuestion || radioWrapper;
 
@@ -269,10 +279,11 @@ document.querySelectorAll("input[type='radio']").forEach((radio) => {
   });
 });
 
+// scrollen bij verkrijger toevoegen
 document.querySelector(".beneficiary").addEventListener("click", () => {
   setTimeout(() => {
-    const allBeneficiaries = document.querySelectorAll(".nestedFieldset");
-    const last = allBeneficiaries[allBeneficiaries.length - 1];
+    const allQuestions = container.querySelectorAll(".optionalQuestion");
+    const last = allQuestions[allQuestions.length - 1];
     last.scrollIntoView({ behavior: "smooth", block: "start" });
   }, 50);
 });
